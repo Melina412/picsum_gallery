b86e2090f56e2ff44966d9bc0dd39145ec01890b
Melina Schild <melina.schild@icloud.com>
2023-10-12 13:06:44 +0200

    js fetch api

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+fetch("https://picsum.photos/v2/list")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    data.forEach((item) => {
+      let id = item.id;
+      let img = item.download_url;
+      let author = item.author;
+      let info = item.url;
+
+      let gallery_item = document.createElement("div");
+
+      // * image
+      let image = document.createElement("img");
+      image.setAttribute("src", img);
+      image.setAttribute("alt", "#");
+      gallery_item.appendChild(image);
+
+      // * author
+      let img_author = document.createElement("p");
+      img_author.textContent = author;
+      gallery_item.appendChild(img_author);
+
+      // * button
+      let see_more = document.createElement("button");
+      see_more.textContent = "see more";
+
+      see_more.addEventListener("click", () => {
+        window.open(info);
+      });
+      gallery_item.appendChild(see_more);
+
+      // * fügt die erstellten divs ins html ein
+      document.querySelector(".gallery").appendChild(gallery_item);
+    });
+  });
